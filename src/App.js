201b6c97030f39db314f241f33d7b60cf457f1fe
Sarang <sarang00005@gmail.com>
2023-08-25 11:40:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=> {
+    fetch('https://dummyjson.com/quotes')
+    .then(response => response.json())
+    .then(value => {
+      console.log(value);
+      Array.isArray(value.quotes)
+      ?setPosts(value.quotes.slice(0,10))
+      :console.log("data is not available");
+    })
+    .catch(error => {
+      console.log("Fetching Data Error:",error);
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='head'>DATA LIST</h1>
+      {posts.map((post, i) =>(
+      <h1 key={i}>{post.quote}</h1>
+      ))}
     </div>
   );
 }
